77191e586fa0b4fe3a78ff315a5d25988b5f1e1f
Git-Prajwal-GH <prajwalhanchinal917@gmail.com>
2025-08-20 16:01:43 +0530

    files edited

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/add")
-def add():
-    a = float(request.args.get("a", 0))
-    b = float(request.args.get("b", 0))
-    return jsonify({"result": a + b})
-
-@app.route("/subtract")
-def subtract():
-    a = float(request.args.get("a", 0))
-    b = float(request.args.get("b", 0))
-    return jsonify({"result": a - b})
-
-@app.route("/multiply")
-def multiply():
-    a = float(request.args.get("a", 0))
-    b = float(request.args.get("b", 0))
-    return jsonify({"result": a * b})
-
-@app.route("/divide")
-def divide():
-    a = float(request.args.get("a", 0))
-    b = float(request.args.get("b", 1))
-    if b == 0:
-        return jsonify({"error": "Division by zero not allowed"}), 400
-    return jsonify({"result": a / b})
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
